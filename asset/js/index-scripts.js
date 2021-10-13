c51012cfa2bd2eefd1b7288533f56ea9b127d625
Nicolas Derobert <derobert.pro@gmail.com>
2021-10-13 22:09:46 +0200

    apres correction url

--- asset/js/index-scripts.js
+++ asset/js/index-scripts.js
@@ -34,7 +34,7 @@ const filterOnHashtag = function (event, val) {
 const mainFunction = async () => {
 	let idOfPage = document.getElementsByTagName("body").id;
 	console.log(idOfPage);
-	if (idOfPage == index) {
+	if (idOfPage == index || idOfPage == "") {
 		urlToApply = urlFromIndex;
 	} else {
 		urlToApply = urlFromOtherPages;
